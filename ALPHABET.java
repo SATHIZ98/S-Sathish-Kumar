7e6b3d504a0c59e0af83dd340fb49081365a63fb
SATHIZ98 <42763694+SATHIZ98@users.noreply.github.com>
2018-09-24 13:59:03 +0530

    Add files via upload

---
+++ ALPHABET.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class ALPHABET
+{
+  public static void main(String[] args) 
+ {
+    Scanner s=new Scanner(System.in);
+   char a;
+   a=s.next().charAt(0);
+   System.out.println();
+   if((a>='a'&&a<='z')||(a>='A'&&a<='Z'))
+    {
+  System.out.println("ALPHABET");
+    }
+   else
+     {
+      System.out.println("NO");
+
+     }
+  }
+}
