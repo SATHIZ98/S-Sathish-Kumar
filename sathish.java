cf56deab13d3c146d0cc7a040463cc39afda2945
SATHIZ98 <42763694+SATHIZ98@users.noreply.github.com>
2018-09-24 10:09:57 +0530

    Add files via upload

---
+++ sathish.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class sathish
+ {
+  public static void main(String[] args)
+  {
+    int a;
+   Scanner s=new Scanner(System.in);
+   a=s.nextInt();
+   if(a>0)
+  {
+    System.out.println("Positive");
+
+  }
+  else 
+  {
+System.out.println(" Negative");
+  }
+  
+  }
+}
