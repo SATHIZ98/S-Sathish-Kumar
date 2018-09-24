df7abaf33fa40acddcd172fbbbadaa73dfb52def
SATHIZ98 <42763694+SATHIZ98@users.noreply.github.com>
2018-09-24 15:41:18 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class helloworld{
+  public static void main(String[] args) {
+   Scanner s=new Scanner(System.in);
+             int a;
+       a=s.nextInt();
+       for(int i=1;i<=a;i++)
+       {
+    System.out.println("Hello world!");
+       }
+  }
+}
